3ba07f9c1f1065d9538e350488d79c79fbbcc7cb
axian <577914237@qq.com>
2021-05-22 22:23:30 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,11 @@
 $(function () {
+  // 在父页面parent.html绑定一个自定义的触发条件和执行方法
+  var $html = $("html");
+  $html.on('parentAndChilenEvent', function () {
+    getUserInfo()
+  });
+
+  var serviceType = 'Object'
   // 调用 getUserInfo 获取用户基本信息
   getUserInfo()
   var layer = layui.layer
@@ -15,48 +22,50 @@ $(function () {
       layer.close(index);
     });
   })
-})
 
-// 获取用户的基本信息
-function getUserInfo() {
-  $.ajax({
-    method: 'GET',
-    url: '/my/userinfo',
-    success: function (res) {
-      if (res.status != 0) {
-        return layer.msg(res.message, { icon: 2 })
-      }
-      // 调用 renderAvatar() 渲染用户的头像
-      renderAvatar(res.data)
-    },
-    // // 不论成功还是失败，都还调用这个 complete这个函数
-    // complete: function (res) {
-    //   // 在 complete 回调函数中，使用 res.responseText 拿到服务器返回的信息
-    //   if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
-    //     // 强制清空 token
-    //     localStorage.removeItem('token')
-    //     // 强制跳转到登录页
-    //     location.href = '/login.html'
-    //   }
-    // }
-  })
-}
-
-// 渲染用户的头像
-function renderAvatar(user) {
-  // 获取用户名称
-  var name = user.nickname || user.username
-  $('#welcome').text('欢迎，' + name)
-  // 按需渲染用户头像
-  if (user.user_pic != null) {
-    // 渲染图片头像
-    $('.layui-nav-img').attr('src', user.user_pic).show()
-    $(',text-avatar').hide()
-  } else {
-    // 渲染文本头像
-    $('.layui-nav-img').hide()
-    var first = name[0].toUpperCase()
-    $('.text-avatar').text(first).show()
+  // 获取用户的基本信息
+  function getUserInfo() {
+    $.ajax({
+      method: 'GET',
+      url: '/my/userinfo',
+      success: function (res) {
+        if (res.status != 0) {
+          return layer.msg(res.message, { icon: 2 })
+        }
+        // 调用 renderAvatar() 渲染用户的头像
+        renderAvatar(res.data)
+      },
+      // // 不论成功还是失败，都还调用这个 complete这个函数
+      // complete: function (res) {
+      //   // 在 complete 回调函数中，使用 res.responseText 拿到服务器返回的信息
+      //   if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+      //     // 强制清空 token
+      //     localStorage.removeItem('token')
+      //     // 强制跳转到登录页
+      //     location.href = '/login.html'
+      //   }
+      // }
+    })
   }
-}
+
+  // 渲染用户的头像
+  function renderAvatar(user) {
+    // 获取用户名称
+    var name = user.nickname || user.username
+    $('#welcome').text('欢迎，' + name)
+    // 按需渲染用户头像
+    if (user.user_pic != null) {
+      // 渲染图片头像
+      $('.layui-nav-img').attr('src', user.user_pic).show()
+      $('.text-avatar').hide()
+    } else {
+      // 渲染文本头像
+      $('.layui-nav-img').hide()
+      var first = name[0].toUpperCase()
+
+      $('.text-avatar').text(first).show()
+    }
+  }
+
+})
 
